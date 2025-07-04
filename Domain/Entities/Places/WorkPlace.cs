82efed40d82bc5fed0eb47bb7b2f39a4bd9e45ec
MestreSol <joaovitorferreira8000@gmail.com>
2025-07-04 15:49:13 -0300

    🚧 Add WorkPlace entity with properties for workplace management

---
+++ Domain/Entities/Places/WorkPlace.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities.Places
+{
+    public class WorkPlace: BaseAuditableEntity
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Abreviation { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Localization Localization { get; set; } = new Localization();
+        public string Floor { get; set; } = string.Empty;
+        public string Building { get; set; } = string.Empty;
+        public int MaxCampacity { get; set; } = 0;  
+        public bool IsActive { get; set; } = true;
+    }
+}
