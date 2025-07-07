ee6657688bee3d77ca66eadb5c15509b21a3e521
MestreSol <joaovitorferreira8000@gmail.com>
2025-07-07 17:10:10 -0300

    ✅ Refactor Permissions class to simplify localization and function properties by removing access distinctions

--- Domain/Entities/Access/Permissions.cs
+++ Domain/Entities/Access/Permissions.cs
@@ -6,35 +6,27 @@ namespace Domain.Entities.Access
     {
         public string Description { get; set; }
 
-        public List<Localization> LocalizationsCanAccess { get; set; }
+        public List<Localization> Localizations { get; set; }
 
-        public List<Localization> LocalizationsCannotAccess { get; set; }
+        public List<Function> Functions{ get; set; }
 
-        public List<Function> FunctionsCanAccess { get; set; }
-
-        public List<Function> FunctionsCannotAccess { get; set; }
 
         public Permissions(string description,
                            List<Localization> localizationsCanAccess,
-                           List<Localization> localizationsCannotAccess,
-                           List<Function> functionsCanAccess,
-                           List<Function> functionsCannotAccess)
+                           List<Function> functionsCanAccess
+                           )
         {
             Description = description;
-            LocalizationsCanAccess = localizationsCanAccess ?? new List<Localization>();
-            LocalizationsCannotAccess = localizationsCannotAccess ?? new List<Localization>();
-            FunctionsCanAccess = functionsCanAccess ?? new List<Function>();
-            FunctionsCannotAccess = functionsCannotAccess ?? new List<Function>();
+            Localizations = localizationsCanAccess;
+            Functions = functionsCanAccess;
             Validate(); 
         }
 
         public void Validate()
         {
             ValidateString(Description, 1, 500, nameof(Description), "Description must be a non-empty string with a maximum length of 500 characters.");
-            ValidateList(LocalizationsCanAccess, nameof(LocalizationsCanAccess));
-            ValidateList(LocalizationsCannotAccess, nameof(LocalizationsCannotAccess));
-            ValidateList(FunctionsCanAccess, nameof(FunctionsCanAccess));
-            ValidateList(FunctionsCannotAccess, nameof(FunctionsCannotAccess));
+            ValidateList(Localizations, nameof(Localizations));
+            ValidateList(Functions, nameof(Functions));
         }
 
        
