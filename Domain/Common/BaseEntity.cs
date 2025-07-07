82275d80c66573c5718d412b519b175e46e4a431
MestreSol <joaovitorferreira8000@gmail.com>
2025-07-07 12:38:47 -0300

    🚧 Add validation methods for string, list, GUID, and object in BaseEntity class

--- Domain/Common/BaseEntity.cs
+++ Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Common
 {
-    public abstract class BaseEntity 
+    public abstract class BaseEntity
     {
         public Guid Id { get; set; }
 
@@ -25,5 +25,36 @@ namespace Domain.Common
         {
             _domainEvents.Clear();
         }
-    }
+        public static void ValidateString(string value, int? minLenght, int maxLength, string? paramName, string? customMessaage)
+        {
+            if (value == null)
+                throw new ArgumentNullException(paramName, customMessaage ?? $"{paramName} cannot be null.");
+            if (value.Length > maxLength)
+                throw new ArgumentException(customMessaage ?? $"{paramName} cannot exceed {maxLength} characters.", paramName);
+            if (minLenght.HasValue && value.Length < minLenght.Value)
+                throw new ArgumentException(customMessaage ?? $"{paramName} must be at least {minLenght.Value} characters long.", paramName);
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException(customMessaage ?? $"{paramName} cannot be empty or whitespace.", paramName);
+        }
+
+        public static void ValidateList<T>(List<T> list, string paramName)
+        {
+            if (list == null)
+                throw new ArgumentNullException(paramName, $"{paramName} cannot be null.");
+            if (list.Count == 0)
+                throw new ArgumentException($"{paramName} cannot be empty.", paramName);
+            if (list.Any(item => item == null))
+                throw new ArgumentException("All items in the list must be non-null.", paramName);
+        }
+
+        public static void ValidateGuid(Guid guid, string paramName, string? customMessage = null)
+        {
+            if (guid == Guid.Empty)
+                throw new ArgumentException(customMessage ?? $"{paramName} must be a valid GUID.", paramName);
+        }
+        public static void ValidateObject(object obj, string paramName, string? customMessage = null)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(paramName, customMessage ?? $"{paramName} cannot be null.");
+        }
 }
