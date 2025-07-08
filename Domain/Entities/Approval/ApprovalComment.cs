399a185655cdb1c310f833f182129bd1767b4688
MestreSol <joaovitorferreira8000@gmail.com>
2025-07-08 10:06:46 -0300

    ✅ Update ApprovalComment properties to remove default values and ensure proper validation

--- Domain/Entities/Approval/ApprovalComment.cs
+++ Domain/Entities/Approval/ApprovalComment.cs
@@ -4,8 +4,22 @@ namespace Domain.Entities.Approval
 {
     public class ApprovalComment : BaseAuditableEntity
     {
-        public string Content { get; set; } = string.Empty;
+        public string Content { get; set; }
         public Guid CommenterId { get; set; }
-        public Employee.Employee Commenter { get; set; } = null!;
+        public Employee.Employee Commenter { get; set; }
+
+        public ApprovalComment(string content, Employee.Employee commenter)
+        {
+            Validate(content, commenter);
+            Content = content;
+            CommenterId = commenter.Id;
+            Commenter = commenter;
+        }
+        public void Validate(string content, Employee.Employee commenter)
+        {
+            ValidateString(content, 1, 500, nameof(content), "Content must be between 1 and 500 characters.");
+            ValidateObject(commenter, nameof(commenter), "Commenter cannot be null.");
+
+        }
     }
 }
